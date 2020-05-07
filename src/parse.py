8ba86ce60e149bad0eb2ed9a033f9e49ad8b21e2
thecant <goncalogames22@gmail.com>
2020-05-07 16:38:00 +0100

    csv paragens to prolog paragens, by python

---
+++ src/parse.py
@@ -0,0 +1,18 @@
+import csv
+
+with open('../data/paragens_de_autocarros_SRCR.csv', 'r') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    for row in spamreader:
+        l =  'paragem(\''
+        l += row[0]+'\', '
+        l += row[1]+', '
+        l += row[2]+', \''
+        l += row[3]+'\', \''
+        l += row[4]+'\', \''
+        l += row[5]+'\', \''
+        l += row[6]+'\', '
+        l += str(row[7].split(','))+', \'' 
+        l += row[8]+'\', \''
+        l += row[9]+'\', \''
+        l += row[10]+'\').'
+        print (l)
