5706011a803e72b15df8a3f6d862914a7e5b0323
thecant <goncalogames22@gmail.com>
2020-05-07 16:52:12 +0100

    argv python

--- src/parse_paragens.py
+++ src/parse_paragens.py
@@ -1,9 +1,9 @@
-import csv
+import csv, sys
 
-with open('../data/paragens_de_autocarros_SRCR.csv', 'r') as csvfile:
+with open(sys.argv[1], 'r') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
     for row in spamreader:
-        l =  'paragem(\''
+        l = 'paragem(\''
         l += row[0]+'\', '
         l += row[1]+', '
         l += row[2]+', \''
@@ -11,7 +11,7 @@ with open('../data/paragens_de_autocarros_SRCR.csv', 'r') as csvfile:
         l += row[4]+'\', \''
         l += row[5]+'\', \''
         l += row[6]+'\', '
-        l += str(row[7].split(','))+', \'' 
+        l += str(row[7].split(','))+', \''
         l += row[8]+'\', \''
         l += row[9]+'\', \''
         l += row[10]+'\').'
