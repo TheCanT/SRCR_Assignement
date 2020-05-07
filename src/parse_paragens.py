d909d4584796d89ae4f0dc6c60e5e7e1cc540dab
thecant <goncalogames22@gmail.com>
2020-05-07 17:22:01 +0100

    Reformat code,

--- src/parse_paragens.py
+++ src/parse_paragens.py
@@ -2,17 +2,11 @@ import csv, sys
 
 with open(sys.argv[1], 'r') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    next(spamreader)
     for row in spamreader:
-        l = 'paragem(\''
-        l += row[0]+'\', '
-        l += row[1]+', '
-        l += row[2]+', \''
-        l += row[3]+'\', \''
-        l += row[4]+'\', \''
-        l += row[5]+'\', \''
-        l += row[6]+'\', '
+        l = 'paragem(\''+row[0]+'\', '
+        l += row[1]+', '+row[2]+', \''+row[3]+'\', \''
+        l += row[4]+'\', \''+row[5]+'\', \''+row[6]+'\', '
         l += str(row[7].split(','))+', \''
-        l += row[8]+'\', \''
-        l += row[9]+'\', \''
-        l += row[10]+'\').'
+        l += row[8]+'\', \''+row[9]+'\', \''+row[10]+'\').'
         print (l)
