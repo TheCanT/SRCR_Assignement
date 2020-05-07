93cc1f543a20b05fc5b727c8baa82a4ce8fab0af
thecant <goncalogames22@gmail.com>
2020-05-07 19:03:45 +0100

    Python parse, check for ascii & if coordinates dont existe assign a '0'

--- src/parse_paragens.py
+++ src/parse_paragens.py
@@ -1,12 +1,23 @@
 import csv, sys
 
+sep = '\''
+
+def ascii_only (input):
+    return ''.join(filter(str.isascii,input))
+
 with open(sys.argv[1], 'r') as csvfile, open(sys.argv[2], 'w') as outfile:
-    spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
-    next(spamreader)
-    for row in spamreader:
-        l = 'paragem(\''+row[0]+'\', '
-        l += row[1]+', '+row[2]+', \''+row[3]+'\', \''
-        l += row[4]+'\', \''+row[5]+'\', \''+row[6]+'\', '
-        l += str(row[7].split(','))+', \''
-        l += row[8]+'\', \''+row[9]+'\', \''+row[10]+'\').\n'
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        l = 'paragem(\''
+        l += ascii_only(row['gid'])+sep+', '
+        l += (ascii_only(row['latitude']) if len(row['latitude']) > 0 else '0') +', '
+        l += (ascii_only(row['longitude']) if len(row['longitude']) > 0 else '0') +', '+sep
+        l += ascii_only(row['Estado de Conservacao'])+sep+', '+sep
+        l += ascii_only(row['Tipo de Abrigo'])+sep+', '+sep
+        l += ascii_only(row['Abrigo com Publicidade?'])+sep+', '+sep
+        l += ascii_only(row['Operadora'])+sep+', '
+        l += ascii_only(str(row['Carreira'].split(',')))+', '+sep
+        l += ascii_only(row['Codigo de Rua'])+sep+', '+sep
+        l += ascii_only(row['Nome da Rua'])+sep+', '+sep
+        l += ascii_only(row['Freguesia'])+sep+').\n'
         outfile.write(l)
